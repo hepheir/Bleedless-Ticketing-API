c520cdbc75495c69ec99b9f8a91f4be725a47b7e
Hepheir <hepheir@gmail.com>
2022-02-13 03:50:19 +0900

    Use config module on driver.py

--- engine/driver.py
+++ engine/driver.py
@@ -1,13 +1,24 @@
-import os
+from os import path
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+from config import config
 
-EXECUTABLE_PATH = os.path.abspath('drivers/chromedriver_mac64_m1-2')
 
-driver: WebDriver
+def createDriver() -> WebDriver:
+    executablePath = config.get('driver', 'driver')
+    executableAbsPath = path.abspath(executablePath)
+    try:
+        driver = webdriver.Chrome(executableAbsPath)
+    except WebDriverException as webDriverException:
+        # macOS 사용자의 경우.
+        # 다음과 같은 에러 메시지가 나타난다면 아래의 방법을 시도해 볼 것.
+        # selenium.common.exceptions.WebDriverException: Message: 'chromedriver_mac64_m1-2.zip' executable may have wrong permissions. Please see https://chromedriver.chromium.org/home
+        # 컨트롤 키(⌃)를 누른채로 크롬드라이버 파일을 1회 실행시켜, 파일에 권환을 부여해볼것.
+        raise webDriverException
+    return driver
 
 
 class DriverForWithStatement:
@@ -19,11 +30,4 @@ class DriverForWithStatement:
         return
 
 
-try:
-    driver = webdriver.Chrome(EXECUTABLE_PATH)
-except WebDriverException as webDriverException:
-    # macOS 사용자의 경우.
-    # 에러 메시지가 다음과 같다면, 아래의 방법을 시도해 볼 것.
-    # selenium.common.exceptions.WebDriverException: Message: 'chromedriver_mac64_m1-2.zip' executable may have wrong permissions. Please see https://chromedriver.chromium.org/home
-    # 컨트롤 키(⌃)를 누른채로 크롬드라이버 파일을 1회 실행시켜, 파일에 권환을 부여해볼것.
-    raise webDriverException
+driver = createDriver()
