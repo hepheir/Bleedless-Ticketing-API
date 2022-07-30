923fc31baff560b9b435b9cc4ff4e52762112ab9
Hepheir <hepheir@gmail.com>
2022-07-30 22:08:56 +0900

    feat: add types for interpark

---
+++ src/providers/interpark/@types/interpark.d.ts
@@ -0,0 +1,27 @@
+interface IfrmSeatDetail extends HTMLIFrameElement {
+}
+
+interface StySeat extends HTMLImageElement {
+    src: string;
+    className: "stySeat";
+    style: CSSStyleDeclaration;
+    title: string;
+    onclick: (this: GlobalEventHandlers, ev: MouseEvent) => any;
+}
+
+interface StySelectSeat extends HTMLImageElement {
+    src: string;
+    id: string;
+    className: "stySelectSeat";
+    style: CSSStyleDeclaration;
+    alt: string;
+    title: string;
+    onclick: (this: GlobalEventHandlers, ev: MouseEvent) => any;
+    seatinfo: string;
+    ri: string;
+    ci: string;
+    sa: string;
+    rg: string;
+    sn: string;
+    value: string;
+}
